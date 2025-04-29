f8f2711b2b26c5ffda54ac50ec00f75530ae6d43
Gleb Ebert <5892266+gl-eb@users.noreply.github.com>
2025-04-29 11:24:05 +0200

    use native version shortcode
    added in Quarto 1.7

--- filters/quarto-version.lua
+++
@@ -1,8 +0,0 @@
--- by Carl Goodwin
--- https://github.com/cgoo4/quantumjitter/blob/main/quarto-version.lua
-
-function RawInline (raw)
-  if raw.format:match 'html' and raw.text == '<?quarto.version?>'then
-    return tostring(quarto.version)
-  end
-end
